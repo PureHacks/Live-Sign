3f8addff80ae0de052bcd35d6c498931ae7142c0
Wil Turrin <wil.turrin@nurun.com>
2014-12-15 14:31:42 -0500

    WT - cleaned up some FE issues

--- public/assets/controllers/mainController.js
+++ public/assets/controllers/mainController.js
@@ -1,63 +1,66 @@
 function MainController($scope,$http){
-	window.mc_scope = $scope;
-	// scope variables
-	$scope.results = [];
+    window.mc_scope = $scope;
+    // scope variables
+    $scope.results = [];
 
     $scope.pageView = "";
+    $scope.dict = window.dictionary;
+    console.log('dicionary: ', $scope.dict);
 
     //constants
     // page titles
     $scope.scheduling = "Scheduling";
-	$scope.campaign = "Campaign";
-	$scope.images = "Images";
+    $scope.campaign = "Campaign";
+    $scope.images = "Images";
 
-	//$scope.saveCampaign = "Save Campaign";
-	//$scope.scheduleCampaign = "Schedule Campaign";
+    $scope.saveImage = "Save Image";
+    $scope.selectImage = "Select Image";
 
-	$scope.setFilterDate = function(date) {
-	    $scope.filterDate = date;
-	}
+    //$scope.saveCampaign = "Save Campaign";
+    //$scope.scheduleCampaign = "Schedule Campaign";
 
-    // set page view to default of 'scheduling'
-    $scope.pageView = $scope.scheduling;
+    $scope.setFilterDate = function(date) {
+        $scope.filterDate = date;
+    }
 
     $scope.changeView = function(viewName){
         console.log('changeView:',viewName);
         $scope.pageView = viewName;
     }
 
-	$scope.init = function() {
-        console.log('i am main controller init');
+    $scope.init = function() {
+        // set page view to default of 'scheduling'
+        $scope.changeView($scope.images);
 
-		// reset list
-		$scope.results = [];
+        // reset list
+        $scope.results = [];
 
-		// get data
+        // get data
         // TODO: tie in to live data
-		/*
-		$scope.results = the results of query
-		*/
+        /*
+         $scope.results = the results of query
+         */
     };
 };
 
 
 /* FILTER DATE EXAMPLE
-app.filter('filterDate', function() {
-	console.log('date:',filterDate);
-	return function(input, filterDate) {
-        if (typeof filterDate == 'undefined' || filterDate == null) {
-            return input;
-        } else {
-            var out = [];
-            for (var a = 0; a < input.length; a++){
-                
-                if(input[a].show.date[b] == filterDate) {
-                    out.push(input[a]);
-                }
-                
-            }
-            return out;
-        }
-    };
-});
-*/
+ app.filter('filterDate', function() {
+ console.log('date:',filterDate);
+ return function(input, filterDate) {
+ if (typeof filterDate == 'undefined' || filterDate == null) {
+ return input;
+ } else {
+ var out = [];
+ for (var a = 0; a < input.length; a++){
+
+ if(input[a].show.date[b] == filterDate) {
+ out.push(input[a]);
+ }
+
+ }
+ return out;
+ }
+ };
+ });
+ */
---
+++ public/assets/lib/dictionary.js
@@ -0,0 +1,7 @@
+window.dictionary = {};
+
+window.dictionary.images = {
+
+    "imageNamePlaceholder" : "Please select an image"
+
+};
