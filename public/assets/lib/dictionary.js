681306a918edc67723d9594ad5420e3f92ad70a8
Matt McFadyen <mmcfadyen91@gmail.com>
2014-12-16 18:36:07 -0500

    reverted to Wil's changes

--- public/assets/lib/dictionary.js
+++ public/assets/lib/dictionary.js
@@ -1,33 +1,34 @@
 window.dictionary = {};
 
 window.dictionary.images = {
-	"addNewImage" : "Add New Image",
-	"imageNamePlaceholder" : "Please select an image",
-	"saveImage" : "Save Image",
-	"selectImage" : "Select Image",
-	"imageName" : "Image Name: ",
+    "addNewImage" : "Add New Image",
+    "imageNamePlaceholder" : "Please select an image",
+    "saveImage" : "Save Image",
+    "selectImage" : "Select Image",
+    "imageName" : "Image Name: ",
     "createdOn" : "Created on",
     "imageList" : "Available Images",
     "imagePreview" : "Preview"
 };
 
 window.dictionary.campaign = {
-	"createCampaignTitle" : "Create a new Campaign",
-	"allCampaigns" : "All Campaigns",
-	"createCampaign" : "Create campaign",
+    "createCampaignTitle" : "Create a new Campaign",
+    "allCampaigns" : "All Campaigns",
+    "createCampaign" : "Create campaign",
     "addImage" : "Add Image",
     "campaignName" : "Campaign Name",
-	"createButton" : "Save Campaign",
-	"scheduleCampaign" : "Schedule Campaign",
+    "createButton" : "Save Campaign",
+    "scheduleCampaign" : "Schedule Campaign",
     "campaignDescription" : "Campaign Description"
 }
 
 window.dictionary.schedule = {
     "scheduleCampaign" : "Schedule a campaign",
-    "showCampaigns" : "Show Campaigns",
+    "showCampaigns" : "Show Scheduled Campaigns",
     "selectCampaign" : "Select Campaign",
-    "createCampaign" : "Create Campaign",
+    "scheduleCampaign" : "Schedule Campaign",
     "campaign" : "Campaign",
     "startTime" : "Starts",
-    "endTime" : "Ends"
+    "endTime" : "Ends",
+    "edit" : "Edit"
 }
